e90924fb79d7094d3e508ed4861750b02f92895a
Administrator <admin2k7@gmail.com>
2025-06-17 15:48:48 +0300

    Fixing error 500

--- src/users/interfaces/views.py
+++ src/users/interfaces/views.py
@@ -1,5 +1,5 @@
 from django.db import IntegrityError
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
