738cc1aae35515d4a7fcc361785326475ccef576
Administrator <admin2k7@gmail.com>
2025-07-14 11:32:27 +0300

    Changed access_token life time

--- src/settings.py
+++ src/settings.py
@@ -111,7 +111,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
