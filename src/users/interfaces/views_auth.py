15befdf821aee3a496639e1de18d35491b545f20
Administrator <admin2k7@gmail.com>
2025-07-14 11:12:03 +0300

    Added logout logic

--- src/users/interfaces/views_auth.py
+++ src/users/interfaces/views_auth.py
@@ -1,27 +1,22 @@
-from django.shortcuts import redirect
-from drf_spectacular.utils import extend_schema
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from shared.auth.service import AuthService
 
 
-@extend_schema(
-    tags=['Users'],
-    responses={200: None}
-)
 class LogoutView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        try:
-            token = request.COOKIES.get("refresh_token")
-            if token:
-                RefreshToken(token).blacklist()
-        except Exception:
-            pass
+        refresh_token = request.COOKIES.get("refresh_token")
+        if refresh_token:
+            try:
+                RefreshToken(refresh_token).blacklist()
+            except Exception:
+                pass
 
-        resp = redirect("/")
+        resp = Response(status=status.HTTP_204_NO_CONTENT)
         AuthService.clear_jwt_cookies(resp)
-
         return resp
