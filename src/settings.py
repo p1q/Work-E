fccd96034c34f8804500f6a5d7d216c7f85352a1
Administrator <admin2k7@gmail.com>
2025-07-15 14:49:02 +0300

    Updated port

--- src/settings.py
+++ src/settings.py
@@ -124,7 +124,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "https://work-e.netlify.app",
     "https://stage--work-e.netlify.app",
-    "https://localhost:3000",
+    "https://localhost:3001",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
