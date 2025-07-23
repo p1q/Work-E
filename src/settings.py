77a99cc13df5be5b1e3c241a6f8ff665e7f637f3
Administrator <admin2k7@gmail.com>
2025-07-23 18:18:11 +0300

    Added DRF-token auth

--- src/settings.py
+++ src/settings.py
@@ -91,6 +91,7 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
